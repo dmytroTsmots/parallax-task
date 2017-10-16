579738cf115b431721fec8f6adbd5f812780524f
dmytroTsmots <dmytro.tsmots@gmail.com>
2017-10-16 15:34:22 +0300

    update parallax task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-window.addEventListener('scroll', function (e) {
-  	var $parallax = document.getElementById('parallax');
-  	var posY = document.body.scrollTop * 0.3;
-  	$parallax.style.backgroundPosition = '0px ' + (-300 + posY) +'px';
+window.addEventListener('scroll', function(e) {
+	console.log(document.body.scrollTop);
+	var $parallax = document.getElementById('parallax');
+	var posY = document.documentElement.scrollTop * 0.3;
+
+	$parallax.style.backgroundPosition = '0px ' + (-300 + posY) + 'px';
 	var $parallax2 = document.getElementById('parallax1');
-  	var posY = document.body.scrollTop * 0.3;
-  	$parallax2.style.backgroundPosition = '0px '+ (-400 + posY) +'px';
-	})
+	var posY = document.documentElement.scrollTop * 0.3;
+	$parallax2.style.backgroundPosition = '0px ' + (-400 + posY) + 'px';
+})
+
+
